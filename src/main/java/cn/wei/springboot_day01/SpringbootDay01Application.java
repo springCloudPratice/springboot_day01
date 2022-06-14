6c0f9f637fa3b288c86221dc81f7d1949a336bab
yaohongweiisme <2558939179@qq.com>
2022-06-14 11:23:35 +0800

    第一次提交，创建项目模板

---
+++ src/main/java/cn/wei/springboot_day01/SpringbootDay01Application.java
@@ -0,0 +1,13 @@
+package cn.wei.springboot_day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDay01Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/wei/springboot_day01/SpringbootDay01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wei.springboot_day01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDay01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
